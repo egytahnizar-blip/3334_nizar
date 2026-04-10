055e23c9764fb483c12848f9539b61e9cf6681c7
Nizar_3334 <egytahnizar@gmail.com>
2026-04-10 11:07:10 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang', function () {
+    return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+});
+
+Route::get('/kontak', function () {
+    return view('contact');
+});
+
