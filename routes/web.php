c1cddf2c6b18eefa23cf4f791eccf49e66b20ddd
Nizar_3334 <egytahnizar@gmail.com>
2026-04-17 16:12:03 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,43 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\EventController as AdminEventController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/profil', function () {
-    return view('profil');
-})->name('profil');
-Route::get('/katalog', function () {
-    return view('katalog');
-})->name('katalog');
-Route::get('/bantuan', function () {
-    return view('bantuan');
-})->name('bantuan');
-Route::get('/kontak', function () {
-    return view('contact');
-})->name('kontak');
-
 Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
 Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
-
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/events', [AdminEventController::class, 'indexAdmin'])->name('events.index');
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
+// dan seterusnya...
 });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/tentang', function() {
+//     return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
+// });
+
+// Route::get('/kontak', function() {
+//     return view('contact');
+// });
+
+// Route::get('/profile', function(){
+//     return view('profile');
+// });
+
+// Route::get('/katalog', function(){
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function(){
+//     return view('bantuan');
+// });
+
+// Route::get('/',)
